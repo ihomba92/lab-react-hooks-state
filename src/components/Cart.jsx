f3def0f85c49b30c468f1755948f365d12f251ae
ihomba92 <ihombakibandidavid@gmail.com>
2026-05-03 18:08:05 +0300

    added React components

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-
-const Cart = () => {
+//import ProductCard from './ProductCard'
+const Cart = ({ cartItems }) => {
   return (
     <div>
       <h2>Shopping Cart</h2>
+      {cartItems.length === 0 ? (
+      <p>Your cart is empty.</p>
+    ) : (      
       <ul>
-        {/* TODO: Include items here in li tags with text 'ITEM.NAME is in your cart.' */}
+        {cartItems.map((item, index) => (<li key={index}>{item.name} is in your cart.</li>))}
       </ul>
+      )}
     </div>
   )
 }
 
 export default Cart
+
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import styles from '../styles/ProductCard.module.css'
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, addToCart }) => {
   return (
     <div
-      className={`${styles.card} ${!product.inStock ? styles.outOfStock : ''}`}
+    data-testid={`product-${product.id}`}
+    className={`${styles.card} ${!product.inStock ? styles.outOfStock : ''}`}
     >
       <h3>{product.name}</h3>
       <p>Price: {product.price}</p>
-      <p>Status: {product.inStock ? 'In Stock' : 'Out of Stock'}</p>
-
-      {/* TODO: Implement Add to Cart button functionality */}
-      <button data-testid={'product-' + product.id}>Add to Cart</button>
+      <button onClick={() => addToCart(product)} disabled={!product.inStock}>Add to Cart</button>
     </div>
   )
 }
