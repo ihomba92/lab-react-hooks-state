479ce47062a92e599302fb12214013d3a2a9e71a
ihomba92 <ihombakibandidavid@gmail.com>
2026-05-04 22:34:12 +0300

    Add data-testid attribute to Add to Cart button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,12 +4,11 @@ import styles from '../styles/ProductCard.module.css'
 const ProductCard = ({ product, addToCart }) => {
   return (
     <div
-    data-testid={`product-${product.id}`}
     className={`${styles.card} ${!product.inStock ? styles.outOfStock : ''}`}
     >
       <h3>{product.name}</h3>
       <p>Price: {product.price}</p>
-      <button onClick={() => addToCart(product)} disabled={!product.inStock}>Add to Cart</button>
+      <button  data-testid={'product-' + product.id} onClick={() => addToCart(product)}>Add to Cart</button>
     </div>
   )
 }
