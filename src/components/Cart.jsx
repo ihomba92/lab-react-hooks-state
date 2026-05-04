f0be908b74dc3a53a78ca878623af2266a782a5a
ihomba92 <ihombakibandidavid@gmail.com>
2026-05-04 20:44:52 +0300

    Remove empty cart message from Cart component
    Removed empty cart message while retaining header.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const Cart = ({ cartItems }) => {
   return (
     <div>
-      <h2>Shopping Cart</h2>
-      {cartItems.length === 0 ? (
-      <p>Your cart is empty.</p>
-    ) : (      
+      <h2>Shopping Cart</h2>     
       <ul>
         {cartItems.map((item, index) => (<li key={index}>{item.name} is in your cart.</li>))}
       </ul>
